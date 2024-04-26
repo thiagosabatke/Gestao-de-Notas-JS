3e35aa2d812492f879386edd5842294c4c9a6d36
ThiagoSabatke <tsabatke7@gmail.com>
2024-04-25 21:54:28 -0300

    commit_thiago

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 function adicionaDadosAlunos() {
-    
+
     let nome = document.getElementById('input_nome').value;
     let ra = document.getElementById('input_ra').value;
     let email = document.getElementById('input_email').value;
@@ -10,18 +10,17 @@ function adicionaDadosAlunos() {
     let prova2 = parseFloat(document.getElementById('input_prova_2').value);
     let aep2 = parseFloat(document.getElementById('input_aep_2').value);
     let integrada2 = parseFloat(document.getElementById('input_prova_integrada_2').value);
-    
-    if ((prova1 < 0 || prova1 > 8) || (prova2 < 0 || prova2 > 8) ||
-        (aep1 < 0 || aep1 > 1) || (aep2 < 0 || aep2 > 1) ||
-        (integrada1 < 0 || integrada1 > 1) || (integrada2 < 0 || integrada2 > 1)) {
-        alert("Por favor, verifique se as notas estão dentro dos limites permitidos.");
+
+    if ((isNaN(prova1)) || (isNaN(prova2)) ||
+        (isNaN(aep1)) || (isNaN(aep2)) ||
+        (isNaN(integrada1)) || (isNaN(integrada2)) || nome == '') {
         return false;
     }
 
     let media1 = (prova1 * 0.8) + (aep1 * 0.1) + (integrada1 * 0.1);
     let media2 = (prova2 * 0.8) + (aep2 * 0.1) + (integrada2 * 0.1);
     let mediaTotal = (media1 + media2) / 2;
-    
+
     let tabela = document.getElementById('tabela');
     let tamanho_tabela = tabela.rows.length;
     let linha = tabela.insertRow(tamanho_tabela);
@@ -46,6 +45,7 @@ function adicionaDadosAlunos() {
         deletarDados(linha)
     };
 
+
     linha.id = "linha_" + tamanho_tabela;
     linha.insertCell(0).innerHTML = tamanho_tabela;
     linha.insertCell(1).innerHTML = nome;
@@ -69,7 +69,7 @@ function adicionaDadosAlunos() {
     document.getElementById('input_prova_2').value = "";
     document.getElementById('input_aep_2').value = "";
     document.getElementById('input_prova_integrada_2').value = "";
-    
+
     return false;
 }
 
@@ -85,9 +85,9 @@ function editarDados(linha) {
     document.getElementById('input_prova_2_edit').value = cells[5].innerHTML;
     document.getElementById('input_aep_2_edit').value = cells[6].innerHTML;
     document.getElementById('input_prova_integrada_2_edit').value = cells[7].innerHTML;
-    
-    document.getElementById('editar_formulario').onsubmit = function(event) {
-        event.preventDefault(); 
+
+    document.getElementById('editar_formulario').onsubmit = function (event) {
+        event.preventDefault();
 
         cells[1].innerHTML = document.getElementById('input_nome_edit').value;
         cells[2].innerHTML = document.getElementById('input_prova_1_edit').value;
@@ -104,11 +104,11 @@ function editarDados(linha) {
         let prova2 = parseFloat(cells[5].innerHTML);
         let aep2 = parseFloat(cells[6].innerHTML);
         let integrada2 = parseFloat(cells[7].innerHTML);
-        
+
         let media1 = (prova1 * 0.8) + (aep1 * 0.1) + (integrada1 * 0.1);
         let media2 = (prova2 * 0.8) + (aep2 * 0.1) + (integrada2 * 0.1);
         let mediaTotal = (media1 + media2) / 2;
-        
+
         cells[8].innerHTML = media1.toFixed(1);
         cells[9].innerHTML = media2.toFixed(1);
         cells[10].innerHTML = mediaTotal.toFixed(1);
@@ -126,3 +126,9 @@ function deletarDados(linha) {
     linha.parentNode.removeChild(linha);
     return false;
 }
+
+
+function Validar(mediaTotal){
+    
+}
+
